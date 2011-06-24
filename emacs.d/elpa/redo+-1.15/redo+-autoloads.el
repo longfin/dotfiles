35097671775ea5230a0c08affabf0e9bfe6283dc
longfin <longfinfunnel@gmail.com>
2011-06-24 13:40:28 +0900

    -add mac-key-mode, redo+, magit-simple-keys

---
+++ emacs.d/elpa/redo+-1.15/redo+-autoloads.el
@@ -0,0 +1,18 @@
+;;; redo+-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+
+;;;### (autoloads nil nil ("redo+-pkg.el" "redo+.el") (19972 4849
+;;;;;;  225798))
+
+;;;***
+
+(provide 'redo+-autoloads)
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; redo+-autoloads.el ends here
